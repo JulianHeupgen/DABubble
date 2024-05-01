14cd7739d15a371f5cf8059d089a3fca9f69a561
JulianHeupgen <julianheupgen@web.de>
2024-05-01 15:25:31 +0200

    added new paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { ChannelChatComponent } from './dashboard/channel-chat/channel-chat.component';
 import { UserChatComponent } from './dashboard/user-chat/user-chat.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent},
@@ -12,6 +14,8 @@ export const routes: Routes = [
       { path: 'channel/:id', component: ChannelChatComponent },
       { path: 'chat/:id', component: UserChatComponent },
     ]
-  }
+  },
+  { path: 'privacy', component: PrivacyComponent},
+  { path: 'imprint', component: ImprintComponent}
 ];
 
