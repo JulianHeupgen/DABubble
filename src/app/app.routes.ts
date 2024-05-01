a52161e4aa7d6cebe73241238dc206a5f0fff722
lubinga <pfatsch@icloud.com>
2024-05-01 08:40:30 +0200

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'dashboard', component:DashboardComponent,
+  }
+];
