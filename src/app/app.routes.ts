af869858aeefe069ed80182e65f3ffab38be2189
JanTHorstmann <janhorstmann@yahoo.de>
2024-05-01 13:17:33 +0200

    login html and css/ mixin.scss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-  { path: '', component: AppComponent},
+  { path: '', component: LoginComponent},
   { path: 'dashboard', component: DashboardComponent}
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,23 +1,44 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldControl, MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
+    CommonModule,
     MatButtonModule,
     MatCardModule,
-    MatInputModule,
-    MatFormFieldModule,
+    MatInputModule,    
     MatIconModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  loginData = this.formBuilder.group({
+    name: '',
+    email: '',
+  })
+
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  logIn(event: Event) {
+    console.log('log in', this.loginData.value);
+    event.preventDefault();
+    
+}
+
+guestLogin() {
+    console.log('Gast Login');
+    
+}
 }
