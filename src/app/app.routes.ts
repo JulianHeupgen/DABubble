3ad1ef379e2f439def0482ba20b8c0181be84d6d
JulianHeupgen <julianheupgen@web.de>
2024-05-01 13:59:32 +0200

    added router outlet to dashboard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,8 +7,7 @@ import { UserChatComponent } from './dashboard/user-chat/user-chat.component';
 export const routes: Routes = [
   { path: '', component: LoginComponent},
   {
-    path: 'dashboard',
-    component: DashboardComponent,
+    path: 'dashboard', component: DashboardComponent,
     children: [
       { path: 'channel/:id', component: ChannelChatComponent },
       { path: 'chat/:id', component: UserChatComponent },
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,11 +4,12 @@ import { UserChatComponent } from './user-chat/user-chat.component';
 import { HeaderComponent } from './header/header.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { ThreadComponent } from './thread/thread.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [HeaderComponent, SidenavComponent, ChannelChatComponent, UserChatComponent, ThreadComponent],
+  imports: [HeaderComponent, SidenavComponent, RouterOutlet, ChannelChatComponent, UserChatComponent, ThreadComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
