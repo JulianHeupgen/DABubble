43d85b72f9995f1a9767ebc6e2f950cecf68b09a
JulianHeupgen <julianheupgen@web.de>
2024-05-01 13:44:47 +0200

    added new paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,17 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { ChannelChatComponent } from './dashboard/channel-chat/channel-chat.component';
+import { UserChatComponent } from './dashboard/user-chat/user-chat.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent},
-  { path: 'dashboard', component: DashboardComponent}
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [
+      { path: 'channel/:id', component: ChannelChatComponent },
+      { path: 'chat/:id', component: UserChatComponent },
+    ]
+  }
 ];
