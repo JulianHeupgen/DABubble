a4a676d71fa400fa0148c563e798e73f59314a89
lubinga <pfatsch@icloud.com>
2024-05-02 17:48:21 +0200

    update

--- src/app/animations/logo/logo.component.ts
+++ src/app/animations/logo/logo.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import {
   trigger,
@@ -6,42 +7,86 @@ import {
   animate,
   transition
 } from '@angular/animations';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logo',
   standalone: true,
-  imports: [],
-  templateUrl: './logo.component.html',
-  styleUrls: [ './logo.component.scss' ],
-  animations: [
-    trigger('openClose', [
-      // ...
-      state('open', style({
-        height: '200px',
-        opacity: 1,
-        backgroundColor: 'yellow'
-      })),
-      state('closed', style({
-        height: '100px',
-        opacity: 0.8,
-        backgroundColor: 'blue',
-        color: 'whitesmoke'
-      })),
-      transition('open => closed', [
-        animate('1s')
-      ]),
-      transition('closed => open', [
-        animate('0.5s')
-      ]),
-    ]),
+  imports: [
+    CommonModule
   ],
+  templateUrl: './logo.component.html',
+  styleUrls: ['./logo.component.scss'],
+  animations: [
+    // trigger for the icon
+    trigger('fade', [
+      state('void', style({ opacity: 0 })),
+      transition('void => *', [
+        animate(2000)
+      ])
+
+    ]),
+
+    //trigger for the text slidein
+    trigger('slideIn', [
+      state('void', style({ marginLeft: '-470px' })),
+      transition('void => *', [
+        animate(400, style({ marginLeft: '0' }))
+      ])
+
+    ]),
+
+    trigger('grow', [
+      state('void', style({ width: '0px' })),
+      transition('void => *', [
+        animate(400, style({ width: '470px' }))
+      ])
+    ]),
+
+    trigger('resizeAndMove', [
+      state('start', style({ top: '50%', left: '50%', transform: 'translate(-50%, -50%) scale(1)' })),
+      state('end', style({ top: '32px', left: '32px', transform: 'scale(.3)', transformOrigin: 'top left' })),
+      transition('* => end', animate('1s ease-in-out'))
+    ])
+
+  ]
 })
+
 export class LogoComponent {
 
-  isOpen = true;
+  startGrowing = false;
+  containerState = 'start';
+  iconVisible = true;
+  textVisible = false;
+  logoVisible = false;
+  defaultBackground = false;
 
-  toggle() {
-    this.isOpen = !this.isOpen;
+  constructor(private route: Router){}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    setTimeout(() => {
+      //change to true when animating
+      this.startGrowing = true;
+    }, 800);
+  }
+
+  growDone() {
+    setTimeout(() => {
+      this.textVisible = true;
+    }, 600);
+  }
+
+  slideDone() {
+    this.containerState = 'end';
+  }
+
+  resizeAndMoveDone() {
+    if (this.containerState === 'end'){
+      this.defaultBackground = true;
+      this.route.navigateByUrl('login');
+    }
   }
 
 }
