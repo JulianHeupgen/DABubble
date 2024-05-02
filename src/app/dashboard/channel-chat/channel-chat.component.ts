66b0faff320e16774893e49a8e1ce3bfb4878187
JulianHeupgen <julianheupgen@web.de>
2024-05-02 16:19:38 +0200

    added channel chat wrapper

--- src/app/dashboard/channel-chat/channel-chat.component.ts
+++ src/app/dashboard/channel-chat/channel-chat.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { MatCard, MatCardContent, MatCardHeader } from '@angular/material/card';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import {MatList, MatListModule} from '@angular/material/list';
+import { Channel } from '../../models/channel.class';
 
 @Component({
   selector: 'app-channel-chat',
   standalone: true,
-  imports: [],
+  imports: [MatCard, MatCardHeader, MatCardContent, MatFormField, MatLabel,  MatList, MatListModule],
   templateUrl: './channel-chat.component.html',
   styleUrl: './channel-chat.component.scss'
 })
 export class ChannelChatComponent {
 
+  currentChannel!: Channel;   // In dieser Variable die Channel Infos von Firebase laden und dann die Inhalte rendern
+
 }
+
