298c38f38377355bf1775e859e875812f2bca6b8
JanTHorstmann <janhorstmann@yahoo.de>
2024-05-02 19:22:54 +0200

    athentication google

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,6 +6,11 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldControl, MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
+import { Firestore } from '@angular/fire/firestore';
+
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { signInWithCredential } from '@angular/fire/auth';
+// import { getAuth } from "firebase/auth";
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -27,18 +32,47 @@ export class LoginComponent {
   loginData = this.formBuilder.group({
     email: '',
     password: '',
+
+    // firebase = require('firebase');
+    // firebaseUi = require('firebaseui');
   })
 
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, firebase: Firestore) {
+
+  }
 
   logIn(event: Event) {
     console.log('log in', this.loginData.value);
     event.preventDefault();
+    this.logInWithGoogle();
+  }
 
-}
+  logInWithGoogle() {
+    const provider = new GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
 
-guestLogin() {
-    console.log('Gast Login');    
-}
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        if (credential) {
+          const token = credential.accessToken;
+          const user = result.user;
+          // ...
+        }
+
+      }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.customData.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  
+  }
+
+  guestLogin() {
+    console.log('Gast Login');
+  }
 }
