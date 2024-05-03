4522e2e9ebc923d12e56c9088b0e69db2894392d
JanTHorstmann <janhorstmann@yahoo.de>
2024-05-04 00:54:51 +0200

    google login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,14 +3,17 @@ import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldControl, MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
-import { Firestore } from '@angular/fire/firestore';
 
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { signInWithCredential } from '@angular/fire/auth';
-// import { getAuth } from "firebase/auth";
+
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { Firestore } from '@angular/fire/firestore';
+import { AuthService } from '../services/auth.service';
+// import { getAuth, signInWithPopup } from '@angular/fire/auth';
+
+
+
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -21,76 +24,59 @@ import { signInWithCredential } from '@angular/fire/auth';
     MatInputModule,
     MatIconModule,
     ReactiveFormsModule,
-
+    
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
-
-
   loginData = this.formBuilder.group({
     email: '',
     password: '',
 
-    // firebase = require('firebase');
-    // firebaseUi = require('firebaseui');
   })
 
 
-  constructor(private formBuilder: FormBuilder, firebase: Firestore, firebaseui: Firestore) {
-    // ui = new firebaseui.auth.AuthUI(firebase.auth());
-  }
+  
+  constructor(
+    private formBuilder: FormBuilder,
+    private firebase: Firestore,
+    private authservice: AuthService,
+  ) { }
+
 
   logIn(event: Event) {
     console.log('log in', this.loginData.value);
-    event.preventDefault();
-    // this.logInWithGoogle();
-
-    const actionCodeSettings = {
-      // URL you want to redirect back to. The domain (www.example.com) for this
-      // URL must be in the authorized domains list in the Firebase Console.
-      url: 'https://localhost',
-      // This must be true.
-      handleCodeInApp: true,
-      iOS: {
-        bundleId: 'com.example.ios'
-      },
-      android: {
-        packageName: this.loginData.value.email,
-        installApp: true,
-        minimumVersion: '12'
-      },
-      dynamicLinkDomain: 'example.page.link'
-    };
-    
+    event.preventDefault()
   }
 
-  logInWithGoogle() {
-    const provider = new GoogleAuthProvider();
+  guestLogin() {
 
-    // provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
 
-    const auth = getAuth();
-    signInWithPopup(auth, provider)
+  }
+
+  // googleProvider = new GoogleAuthProvider();
+  // googleAuth = getAuth();
+
+
+  signInWidthGoogle() {
+// this.authservice.signInWidthGoogle();
+    const googleProvider = new GoogleAuthProvider();
+    const googleAuth = getAuth();
+
+    signInWithPopup(googleAuth, googleProvider)
       .then((result) => {
         const credential = GoogleAuthProvider.credentialFromResult(result);
         if (credential) {
           const token = credential.accessToken;
           const user = result.user;
         }
-        // ...
       }).catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
         const email = error.customData.email;
         const credential = GoogleAuthProvider.credentialFromError(error);
       });
-
-  }
-
-  guestLogin() {
-    console.log('Gast Login');
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@angular/fire/auth';
-
+import { Firestore } from '@angular/fire/firestore';
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private auth: Auth) { }
+  constructor(
+    private auth: Auth,
+    private firebase: Firestore
+  ) { }
 
   async signUp(email: string, password: string) {
     try {
@@ -25,6 +29,25 @@ export class AuthService {
     } catch (error) {
       throw error;
     }
+  }
+
+  signInWidthGoogle() {
+    const googleProvider = new GoogleAuthProvider();
+    const googleAuth = getAuth();
+
+    signInWithPopup(googleAuth, googleProvider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        if (credential) {
+          const token = credential.accessToken;
+          const user = result.user;
+        }
+      }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.customData.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+      });
   }
 
 }
