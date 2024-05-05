45b6cf4713c4a182282d882982df08ea84bb139a
JulianHeupgen <julianheupgen@web.de>
2024-05-05 12:36:28 +0200

    fixed bugs

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -9,7 +9,7 @@ export class StorageService {
   
   async uploadFile(file: File) {
     const storage = getStorage();
-    const storageRef = ref(storage, "file");      // 2. Parameter ist nur Test (muss noch angepasst werden)
+    const storageRef = ref(storage, "file");      // 2. Parameter "file" ist nur Test, muss noch angepasst werden: stattdessen file.name
     try {
       const uploadFile = await uploadBytes(storageRef, file);
       const downloadURL: string = await getDownloadURL(uploadFile.ref);
