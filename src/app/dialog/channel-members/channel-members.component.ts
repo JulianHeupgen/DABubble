5e2ea81b3b7f2d0616fee9234e22cc52e78f78c2
DanielSchn <d.schneider@dsc-mail.de>
2024-05-17 12:54:24 +0200

    add specific user to channel by create channel

--- src/app/dialog/channel-members/channel-members.component.ts
+++ src/app/dialog/channel-members/channel-members.component.ts
@@ -4,8 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { MatRadioModule } from '@angular/material/radio';
 import { from, switchMap } from 'rxjs';
-import { Firestore, collection, getDocs, writeBatch, arrayUnion } from '@angular/fire/firestore';
+import { Firestore, collection, getDocs, writeBatch, arrayUnion, doc } from '@angular/fire/firestore';
 import { MatSelectModule } from '@angular/material/select';
+import { DataService } from '../../services/data.service';
 
 
 
@@ -24,11 +25,13 @@ import { MatSelectModule } from '@angular/material/select';
 export class ChannelMembersComponent {
 
   selectedOption = 'all';
+  selectedUsers: string[] = [];
 
   constructor(
     private firestore: Firestore,
     private dialogRef: MatDialogRef<ChannelMembersComponent>,
     public dialog: MatDialog,
+    public dataService: DataService,
     @Inject(MAT_DIALOG_DATA) public data: { channelId: string }
   ) { }
 
@@ -47,20 +50,57 @@ export class ChannelMembersComponent {
     );
   }
 
+  addChannelToSpecificUsers() {
+    const batch = writeBatch(this.firestore);
+    this.selectedUsers.forEach(userId => {
+      const userDocRef = doc(this.firestore, `users/${userId}`);
+      batch.update(userDocRef, { channels: arrayUnion(this.data.channelId) });
+    });
+    return from(batch.commit());
+  }
+
 
   save() {
     if (!this.data.channelId) {
-      console.error('Keine Kanal-ID verfügbar.');
+      console.error('Keine ID verfügbar!');
       return;
     }
-    this.addChannelToAllUsers().subscribe({
-      next: () => {
-        console.log('Kanal-ID wurde erfolgreich zu allen Benutzern hinzugefügt.');
-        this.dialogRef.close();
-      },
-      error: (error) => {
-        console.error('Fehler beim Hinzufügen der Kanal-ID zu allen Benutzern:', error);
-      }
-    });
+    if (this.selectedOption === 'all') {
+      this.addChannelToAllUsers().subscribe({
+        next: () => {
+          console.log('Kanal ID wurde erfolgreich allen Nutzern hinzugefügt!');
+          this.dialogRef.close();
+        },
+        error: (error) => {
+          console.error('Fehler beim hinzufügen der Kanal ID!', error);
+        }
+      });
+    } else if (this.selectedOption === 'specific') {
+      this.addChannelToSpecificUsers().subscribe({
+        next: () => {
+          console.log('Kanal ID wurde erfolgreich den ausgewählten Nutzern hinzugefügt!');
+          this.dialogRef.close();
+        },
+        error: (error) => {
+          console.error('Fehler beim hinzufügen der Kanal ID!', error);
+        }
+      });
+    }
   }
+
+  // save() {
+  //   if (!this.data.channelId) {
+  //     console.error('Keine Kanal-ID verfügbar.');
+  //     return;
+  //   }
+  //   this.addChannelToAllUsers().subscribe({
+  //     next: () => {
+  //       console.log('Kanal-ID wurde erfolgreich zu allen Benutzern hinzugefügt.');
+  //       this.dialogRef.close();
+  //     },
+  //     error: (error) => {
+  //       console.error('Fehler beim Hinzufügen der Kanal-ID zu allen Benutzern:', error);
+  //     }
+  //   });
+  // }
 }
