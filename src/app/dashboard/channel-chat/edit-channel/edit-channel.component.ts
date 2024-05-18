9510a8a3c002438d50c007302aaf5372d1d670cf
JulianHeupgen <julianheupgen@web.de>
2024-05-18 12:53:57 +0200

    added editChannel component

--- src/app/dashboard/channel-chat/edit-channel/edit-channel.component.ts
+++ src/app/dashboard/channel-chat/edit-channel/edit-channel.component.ts
@@ -1,8 +1,9 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Channel } from '../../../models/channel.class';
-import { MatFormField, MatFormFieldControl, MatLabel } from '@angular/material/form-field';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
+import { User } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-edit-channel',
@@ -14,15 +15,17 @@ import { MatInputModule } from '@angular/material/input';
 export class EditChannelComponent {
 
   @Input() currentChannel!: Channel;
-  @Output() leaveChannelEvent = new EventEmitter<void>();
+  @Input() currentUser!: User;
+  @Input() channelId!: string;
 
   title: string = '';
   description: string = '';
 
   ngOnInit() {
-    if (this.currentChannel) {
+    if (this.currentChannel && this.currentUser && this.channelId) {
       this.title = this.currentChannel.title || '';
       this.description = this.currentChannel.description || '';
+      console.log(this.currentUser);
     }
   }
 
@@ -39,7 +42,7 @@ export class EditChannelComponent {
   }
 
   leaveChannel() {
-    this.leaveChannelEvent.emit();
+    console.log("Hello");
+    // this.currentUser.leaveChannel(this.channelId, this.currentChannel);
   }
-
 }
