7ff77814541e788bd8e41bb5c47a0f5297d66a17
JanTHorstmann <janhorstmann@yahoo.de>
2024-05-23 12:48:27 +0200

    update Full Thread with MessageownerId

--- src/app/dashboard/full-thread/full-thread-message/full-thread-message.component.ts
+++ src/app/dashboard/full-thread/full-thread-message/full-thread-message.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { Thread } from '../../../models/thread.class';
 import { EmojiMartComponent } from '../../emoji-mart/emoji-mart.component';
 import { CommonModule } from '@angular/common';
+import { DataService } from '../../../services/data.service';
+import { Message } from '../../../models/message.class';
 
 @Component({
   selector: 'app-full-thread-message',
@@ -19,15 +21,32 @@ import { CommonModule } from '@angular/common';
 export class FullThreadMessageComponent {
 
   @Input() thread!: Thread;
-  threadMessages!: any;
+  threadMessages: any[] = [];
 
+  constructor(
+    private dataService: DataService,
+  ) { }
+
+  ngOnInit() {
+    this.threadMessages = []
+    this.thread.messages.forEach( message => {
+      this.dataService.allUsers.forEach( user => {
+        if (user.id == message.senderId) {
+          message.sender = user;
+        }
+      })
+      this.threadMessages.push(message)
+    })
+    console.log('threadMessages', this.threadMessages);
+    
+  }
 
   getFormattedDatestamp(timestamp: number): any {
     const date = new Date(timestamp);
     const year = date.getFullYear();
     const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Monate sind 0-basiert
     const day = date.getDate().toString().padStart(2, '0');
-    
+
     const formattedDate = `${year}-${month}-${day}`;
     return formattedDate;
   }
@@ -44,7 +63,7 @@ export class FullThreadMessageComponent {
 
   getMessageContent(element: any) {
     console.log('Message Element:', element);
-    
+
   }
 
 }
