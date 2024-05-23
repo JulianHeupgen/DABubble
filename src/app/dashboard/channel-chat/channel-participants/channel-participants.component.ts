0ed67a9c04aa445346b26488c152803e0fb684d2
JulianHeupgen <julianheupgen@web.de>
2024-05-23 18:07:16 +0200

    added function getChannelParticipants

--- src/app/dashboard/channel-chat/channel-participants/channel-participants.component.ts
+++ src/app/dashboard/channel-chat/channel-participants/channel-participants.component.ts
@@ -1,18 +1,41 @@
 import { Component, Input } from '@angular/core';
 import { Channel } from '../../../models/channel.class';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-channel-participants',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './channel-participants.component.html',
   styleUrl: './channel-participants.component.scss'
 })
 export class ChannelParticipantsComponent {
 
   @Input() currentChannel!: Channel;
+  @Input() users!: any;
+
+
+  channelParticipants!: any[];
   
-  
+
+  ngOnInit() {
+    this.getChannelParticipants();
+  }
+
+
+  getChannelParticipants() {
+    this.channelParticipants = [];
+    this.currentChannel.participants.forEach((participant) => {
+      this.users.some((user: any) => {   // some statt forEach für bessere Performance (vorzeitiger Abbruch der inneren Schleife wenn true returned wird)
+        if(participant == user.id) {
+          this.channelParticipants.push(user);
+          return true;           // gesuchter User gefunden; innere Schleife kann vorzeitig beendet werden; weitere User müssen nicht verglichen werden !
+        }
+        return false;
+      });
+    })
+  }
+
 
 }
 
