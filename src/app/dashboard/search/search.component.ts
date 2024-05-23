6b58a51a73ff4ad041154ca145e9e58d15bd6a8a
DanielSchn <d.schneider@dsc-mail.de>
2024-05-23 13:00:27 +0200

    first search function

--- src/app/dashboard/search/search.component.ts
+++ src/app/dashboard/search/search.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Firestore, collection, onSnapshot, query, where } from '@angular/fire/firestore';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
@@ -10,12 +11,27 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
   styleUrl: './search.component.scss'
 })
 export class SearchComponent {
-
+  firestore: Firestore = inject(Firestore);
 
   // Test search component START
   control = new FormControl('');
   streets: string[] = ['Champs-Élysées', 'Lombard Street', 'Abbey Road', 'Fifth Avenue'];
   isPanelOpen: boolean = false;
   // Test Search END
+  users: string[] = [];
+
+
+testTest() {
+  const searchItem = this.control.value;
+  const q = query(collection(this.firestore, 'users'), where('name', '==', searchItem));
+  const unsubscribe = onSnapshot(q, (querySnapshot) => {
+    this.users = [];
+    querySnapshot.forEach((doc) => {
+      this.users.push(doc.data()['authUserId']);
+    });
+    console.log('Users =', this.users.join(','));
+    
+  });
+}
 
 }
