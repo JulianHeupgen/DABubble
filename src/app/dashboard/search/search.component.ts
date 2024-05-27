3ec7fbaecd03d06e348bb6bb4f447b4097de27c1
DanielSchn <d.schneider@dsc-mail.de>
2024-05-27 12:23:45 +0200

    searchbar open profile

--- src/app/dashboard/search/search.component.ts
+++ src/app/dashboard/search/search.component.ts
@@ -2,24 +2,36 @@ import { Component, inject } from '@angular/core';
 import { Firestore, collection, onSnapshot, query, where } from '@angular/fire/firestore';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { DataService } from '../../services/data.service';
+import { User } from '../../models/user.class';
+import { Observable, map, startWith } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+import { ViewProfileComponent } from '../../dialog/view-profile/view-profile.component';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [ReactiveFormsModule, MatAutocompleteModule],
+  imports: [ReactiveFormsModule, MatAutocompleteModule, CommonModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
 export class SearchComponent {
   firestore: Firestore = inject(Firestore);
-
-  // Test search component START
   control = new FormControl('');
-  // streets: string[] = ['Champs-Élysées', 'Lombard Street', 'Abbey Road', 'Fifth Avenue'];
   isPanelOpen: boolean = false;
-  // Test Search END
-  users: string[] = [];
+  users: User[] = [];
 
+  
+
+  constructor(
+    public dataService: DataService,
+    public dialog: MatDialog
+  ) {}
+
+  showUsers() {
+    return this.dataService.allUsers;
+  }
 
 testTest() {
   const searchItem = this.control.value;
@@ -32,6 +44,13 @@ testTest() {
     console.log('Users =', this.users.join(','));
     
   });
+}
+
+openProfile(participant: any) {
+  this.dialog.open(ViewProfileComponent, {
+    data: participant
+  }
+ );
 }
 
 }
