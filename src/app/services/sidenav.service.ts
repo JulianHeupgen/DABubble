944fbab0dd2fc208151f5e966bbb71602cf4985e
DanielSchn <d.schneider@dsc-mail.de>
2024-06-14 17:27:35 +0200

    refactoring and jsdoc

--- src/app/services/sidenav.service.ts
+++ src/app/services/sidenav.service.ts
@@ -12,35 +12,57 @@ export class SidenavService {
   screenIsSmall: boolean = false;
 
 
-  constructor(private dataService: DataService) {
+  constructor() {
     this.updateScreenSize();
-   }
+  }
 
 
-   /**
-   * Toggle variable for sidenav to open or close.
-   */
-    toggleSidenav(value: string) {
-      if (value === 'sidenav') {
-        this.opened = !this.opened;
-      }
-      if (value === 'channels') {
-        this.showChannels = !this.showChannels;
-      }
-      if (value === 'private') {
-        this.showDirectMessages = !this.showDirectMessages;
-      }
+  /**
+  * Toggles the state of various UI components based on the provided value.
+  * This method manages the open state of sidenav elements including the main sidenav,
+  * channels list, and private messages. It checks the provided value to determine
+  * which component's visibility should be toggled.
+  *
+  * @param {string} value - The component identifier to toggle. Valid values are
+  *                         'sidenav', 'channels', and 'private', each corresponding
+  *                         to different parts of the UI.
+  */
+  toggleSidenav(value: string) {
+    if (value === 'sidenav') {
+      this.opened = !this.opened;
     }
-
-
-    toggleSidenavIfScreenIsSmall(value: string) {
-      if (this.windowWidth <= 650) {
-        this.toggleSidenav(value);
-      }
+    if (value === 'channels') {
+      this.showChannels = !this.showChannels;
     }
-
-
-    updateScreenSize() {
-      this.screenIsSmall = window.innerWidth <= 650;
+    if (value === 'private') {
+      this.showDirectMessages = !this.showDirectMessages;
     }
+  }
+
+
+  /**
+  * Toggles the sidenav based on the current window width.
+  * This method checks if the window width is 650 pixels or less,
+  * and if true, it delegates the toggle action to the `toggleSidenav` method.
+  * It is used to conditionally toggle sidenav elements based on the screen size,
+  * ensuring that UI adjustments are responsive.
+  *
+  * @param {string} value - The identifier for the sidenav component to be toggled.
+  */
+  toggleSidenavIfScreenIsSmall(value: string) {
+    if (this.windowWidth <= 650) {
+      this.toggleSidenav(value);
+    }
+  }
+
+
+  /**
+  * Updates the `screenIsSmall` property based on the current window width.
+  * Sets `screenIsSmall` to true if the window width is 650 pixels or less,
+  * otherwise sets it to false. This method is typically used to adjust UI elements
+  * based on the screen size for responsive design purposes.
+  */
+  updateScreenSize() {
+    this.screenIsSmall = window.innerWidth <= 650;
+  }
 }
