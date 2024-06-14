60c2bc795c660dde986262500e428ee28a185394
DanielSchn <d.schneider@dsc-mail.de>
2024-06-14 15:55:41 +0200

    add sidenav service and refactoring

--- src/app/dashboard/header/header.component.ts
+++ src/app/dashboard/header/header.component.ts
@@ -16,6 +16,7 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
 import { SearchComponent } from '../search/search.component';
 import {ChannelChatComponent} from "../channel-chat/channel-chat.component";
 import { DataService } from '../../services/data.service';
+import { SidenavService } from '../../services/sidenav.service';
 
 @Component({
   selector: 'app-header',
@@ -70,7 +71,8 @@ export class HeaderComponent {
     private auth: AuthService,
     private router: Router,
     private profileService: HeaderProfileService,
-    public dataService: DataService
+    public dataService: DataService,
+    public sidenavService: SidenavService
   ) {
     this.subProfileView();
     this.subProfileEdit();
---
+++ src/app/services/sidenav.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { DataService } from './data.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SidenavService {
+
+  opened: boolean = true;
+  showChannels: boolean = true;
+  showDirectMessages: boolean = true;
+
+  constructor(private dataService: DataService) { }
+
+    /**
+   * Toggle variable for sidenav to open or close.
+   */
+    toggleSidenav(value: string) {
+      if (value === 'sidenav') {
+        this.opened = !this.opened;
+      }
+      if (value === 'channels') {
+        this.showChannels = !this.showChannels;
+      }
+      if (value === 'private') {
+        this.showDirectMessages = !this.showDirectMessages;
+      }
+    }
+}
