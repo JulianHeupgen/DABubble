7aa729e0881684dccc222cc8dade13ea4303f15c
DanielSchn <d.schneider@dsc-mail.de>
2024-06-18 11:55:07 +0200

    sidenav trigger from view profile smaller 650px

--- src/app/dialog/view-profile/view-profile.component.ts
+++ src/app/dialog/view-profile/view-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog, MatDialogContent } from '@angular/material/dialog';
 import { RouterLink } from '@angular/router';
+import { SidenavService } from '../../services/sidenav.service';
 
 @Component({
   selector: 'app-view-profile',
@@ -11,11 +12,14 @@ import { RouterLink } from '@angular/router';
 })
 export class ViewProfileComponent {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, public dialog: MatDialog) {}
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, public dialog: MatDialog, private sidenavService: SidenavService) { }
 
 
   closeProfile() {
     this.dialog.closeAll();
+    if (window.innerWidth < 650) {
+      this.sidenavService.toggleSidenavIfScreenIsSmall('sidenav');
+    }
   }
 
 }
