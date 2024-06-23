d45e2aa3cc974f1392a600f3907da8b09921b55a
JulianHeupgen <julianheupgen@web.de>
2024-06-23 15:19:56 +0200

    JSDoc

--- src/app/services/thread.service.ts
+++ src/app/services/thread.service.ts
@@ -20,17 +20,21 @@ export class ThreadService {
   updateThread$ = this.threadChangesSource.asObservable();
   currentMessages$ = this.threadMessageSource.asObservable();
   openCurrentThread$ = this.openCurrentFullThreadSource.asObservable();
-  
-  // isCurrentUser: boolean = false;
-  // openThread: boolean = false;
 
   constructor(
     private dataService: DataService,
   ) { }
 
-
-  //subrcribes//
   
+  /**
+   * Changes the current thread and notifies observers.
+   *
+   * @param {Thread} thread - The thread to switch to.
+   * @param {User} threadOwner - The owner of the thread.
+   * @param {Channel} currentChannel - The current channel.
+   * @param {User} currentUser - The current user.
+   * @returns {Promise<void>} - A promise that resolves when the thread change is complete.
+   */
   changeThread(thread: Thread, threadOwner: User, currentChannel: Channel, currentUser: User): Promise<void> {
     return new Promise((resolve) => {
       this.threadSource.next({ thread, threadOwner, currentChannel, currentUser });
@@ -38,6 +42,13 @@ export class ThreadService {
     });
   }
 
+
+  /**
+   * Opens or closes the full thread view.
+   *
+   * @param {boolean} openFullThreadBoolean - Whether to open or close the full thread view.
+   * @returns {Promise<void>} - A promise that resolves when the operation is complete.
+   */
   openFullThread(openFullThreadBoolean: boolean): Promise<void> {
     return new Promise((resolve) => {
       this.openCurrentFullThreadSource.next(openFullThreadBoolean);
@@ -45,21 +56,34 @@ export class ThreadService {
     })
   }
 
+
+   /**
+   * Sends a request to update reactions for a message in a thread.
+   *
+   * @param {Thread} thread - The thread containing the message.
+   */
   getReactionsForMessage(thread: Thread) {
     let update = 'updateReaction'
     this.threadMessageSource.next({ thread, update });
   }
 
+
+   /**
+   * Sends a request to update the changes in a thread.
+   *
+   * @param {Thread} thread - The thread to update.
+   */
   getThreadChanges(thread: Thread) {
     let update = 'updateThread'
     this.threadChangesSource.next({ thread, update });
   }
 
 
-  //Functions//
-
-
-
+  /**
+   * Copies a thread and updates it in Firebase.
+   *
+   * @param {Thread} originThread - The original thread to copy and update.
+   */
   copyThreadForFirebase(originThread: Thread) {
     const threadCopy = new Thread({ ...originThread });
     threadCopy.messages = [...originThread.messages];
@@ -71,21 +95,40 @@ export class ThreadService {
     });
   }
 
+
+   /**
+   * Converts thread messages to strings for Firebase.
+   *
+   * @param {Thread} thread - The thread whose messages need to be converted.
+   */
   convertThreadMessagesToString(thread: any) {
     thread.messages = thread.messages.map((message: any) => JSON.stringify(message));
   }
 
+
+  /**
+   * Deletes a file associated with a message from the storage.
+   *
+   * @param {string} imgFileLink - The link to the image file to delete.
+   */
   deletFileOfMessage(imgFileLink: string) {
     const storage = getStorage();
     const desertRef = ref(storage, imgFileLink);
     deleteObject(desertRef).then(() => {
       return
     }).catch((error) => {
-      // Uh-oh, an error occurred!
+      console.log(error);
     }); 
   }
 
 
+  /**
+   * Copies a user chat thread and updates it in Firebase.
+   *
+   * @param {Thread} originThread - The original thread to copy and update.
+   * @param {string | undefined} userChatId - The ID of the user chat.
+   * @param {number | undefined} index - The index of the thread in the user chat.
+   */
   copyUserChatThreadForFirebase(originThread: Thread, userChatId: string | undefined, index: number | undefined) {
     const threadCopy = new Thread({ ...originThread });
     threadCopy.messages = [...originThread.messages];
@@ -98,3 +141,4 @@ export class ThreadService {
     });
   }
 }
+
